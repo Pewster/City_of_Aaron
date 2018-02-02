1e6c3cf69295d11dbb2b815e21423f446eabe2a8
pewst <pewst@192.168.3.106>
2018-02-01 21:02:45 -0700

    Team members

--- findTheGold/src/CIT260/Lesson4/TeamMembers.java
+++ findTheGold/src/CIT260/Lesson4/TeamMembers.java
@@ -5,23 +5,19 @@
  */
 package CIT260.Lesson4;
 
+import java.util.Objects;
+
 /**
  *
  * @author pewst
  */
-public enum TeamMembers {
+public class TeamMembers {
     
     // enum attributes
     private String name;
+    private String title;
     
-    // constructors
-
-    private TeamMembers(String name) {
-        this.name = name;
-    }
-    
-
-    //getters and setters
+   //getters and setters
 
     public String getName() {
         return name;
@@ -30,6 +26,55 @@ public enum TeamMembers {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    // equals and hashcodes
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.title);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TeamMembers other = (TeamMembers) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        return true;
+        }
+    
+        // to string
+
+    @Override
+    public String toString() {
+        return "TeamMembers{" + "name=" + name + ", title=" + title + '}';
+    }
+        
+       
+    
         
     
 
