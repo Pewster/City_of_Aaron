7ac7dbeb86fe04a1ef1230f62ea8689d9b4e9a77
pewst <pewst@192.168.3.106>
2018-02-15 19:35:25 -0700

    Hayden

---
+++ Couriousworkmanship/src/findthegold/FindTheGold.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findthegold;
+
+import CIT260.Lesson4.Player;
+
+/**
+ *
+ * @author pewst
+ */
+public class FindTheGold {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        player)One.setName("Fred Flintstone");
+        
+        String playerOneName = playerOne.getName();
+        
+        System.out.println("Name = " + playerOneName)
+        
+    }
+    
+}
