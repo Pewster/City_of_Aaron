20d896cd5ef89de039a78517d89f278f18e2b426
pewst <pewst@192.168.3.106>
2018-02-28 20:50:11 -0700

    Hayden

---
+++ CityOfAaron/src/beans/TeamMembers.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author pewst
+ */
+
+public /**enum**/ class TeamMembers {
+
+    // enum attributes
+    private String name;
+    private String title;
+    
+    // constructors
+    private TeamMembers(String name) {
+        this.name = name;
+    }
+   
+    //getters and setters
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    // equals and hashcodes
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.title);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TeamMembers other = (TeamMembers) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        return true;
+        }
+    
+        // to string
+
+    @Override
+    public String toString() {
+        return "TeamMembers{" + "name=" + name + ", title=" + title + '}';
+    }
+}
